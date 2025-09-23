2701754f377ef9672c6b8dca3d5980be3c116b74
gitaud <denisgitau12@gmail.com>
2025-09-23 13:48:18 +0300

    Create Hero with image slider

---
+++ src/heros/HomeHero/config.ts
@@ -0,0 +1,37 @@
+
+import type { Field } from 'payload'
+
+export const heroSlider: Field = {
+  name: 'heroSlider',
+  type: 'group',
+  fields: [
+    {
+      name: 'headline',
+      type: 'text',
+      label: 'Headline'
+    },
+    {
+      name: 'cta_label',
+      type: 'text',
+      label: 'CTA Label'
+    },
+    {
+      name: 'cta_link',
+      type: 'text',
+      label: 'CTA Link'
+    },
+    {
+      name: 'images',
+      label: 'Images',
+      type: 'array',
+      fields: [
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+          required: true
+        }
+      ]
+    }
+  ]
+}
---
+++ src/heros/HomeHero/index.tsx
@@ -0,0 +1,12 @@
+import { Page } from "@/payload-types";
+import React from "react";
+
+export const HomeHero: React.FC<Page['hero']> = ({
+  links, media, richText
+}) => {
+  return(
+    <>
+      
+    </>
+  )
+}
