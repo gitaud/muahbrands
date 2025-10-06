b30a6a81520bbb6299fe7ed03116fa229f486589
gitaud <denisgitau12@gmail.com>
2025-10-06 11:41:03 +0300

    Rm cta from hero

--- src/components/HeroSlider/index.tsx
+++ src/components/HeroSlider/index.tsx
@@ -34,12 +34,12 @@ export function ImagesSliderComponent(
         <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
           {headline}
         </motion.p>
-        <a  href={cta_link!}>
+        {/* <a  href={cta_link!}>
           <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
               <span>{cta_label} →</span>
               <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
           </button>
-        </a>
+        </a> */}
       </motion.div>
     </ImagesSlider>
   );
